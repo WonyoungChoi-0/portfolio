9f1b8c67e7f5ba779c5282b023c0dd21d7e842b6
Wonyoung Choi <wc7qpe@virginia.edu>
2021-02-13 22:24:47 -0500

    Utilized gatsby-background-image

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types"
 import React, { useState, useEffect } from "react"
-import Img from "gatsby-image"
 import {Link} from "react-scroll"
+import BackgroundImage from 'gatsby-background-image'
 
 import {
   Collapse,
@@ -16,16 +16,13 @@ import {
 const Header = ({headerImage}) => {
   const [isOpen, setIsOpen] = useState(false);
   const [header, setHeader] = useState("header-at-top")
-  const [link, setLink] = useState("nav-link-top")
 
   const toggle = () => setIsOpen(!isOpen);
 
   const listenScrollEvent = (event) => {
     if (window.scrollY < 5) {
-      setLink("nav-link-top")
       return setHeader("header-at-top")
     } else if (window.scrollY > 5) {
-      setLink(" ")
       return setHeader("header-free")
     } 
   }
@@ -40,30 +37,31 @@ const Header = ({headerImage}) => {
   return (
     <header id="homeSection">
       <Navbar fixed="top" light expand="sm" className={header}>
-        <div className="container">
-          <NavbarToggler onClick={toggle}/>
-          <Collapse isOpen={isOpen} navbar>
-            <Nav className="mr-auto" navbar>
-              <NavItem style={{marginRight: "5%"}}>
-                <Link to="homeSection" className="nav-link" style={{fontSize: "18px", cursor: "pointer"}} spy={true} smooth={true} duration={1000}>HOME</Link>
-              </NavItem>
-              <NavItem style={{marginRight: "5%"}}>
-                <Link to="aboutSection" className="nav-link" style={{fontSize: "18px", cursor: "pointer"}} offset={30} spy={true} smooth={true} duration={1000}>ABOUT</Link>
-              </NavItem>
-              <NavItem style={{marginRight: "5%"}}>
-                <Link to="portfolioSection" className="nav-link" style={{fontSize: "18px", cursor: "pointer"}} offset={30} spy={true} smooth={true} duration={1000}>PORTFOLIO</Link>
-              </NavItem>
-              <NavItem style={{marginRight: "5%"}}>
-                <Link to="blogSection" className="nav-link" style={{fontSize: "18px", cursor: "pointer"}} offset={-50} spy={true} smooth={true} duration={1000}>BLOG</Link>
-              </NavItem>
-              <NavItem style={{marginRight: "5%"}}>
-                <Link to="footerSection" className="nav-link" style={{fontSize: "18px", cursor: "pointer"}} spy={true} smooth={true} duration={1000}>CONTACT</Link>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </div>
-      </Navbar>
-      <Img fluid={headerImage.childImageSharp.fluid} style={{width: "98.9vw", height: "100vh"}} alt="Background Image"/>
+          <div className="container">
+            <NavbarToggler onClick={toggle}/>
+            <Collapse isOpen={isOpen} navbar>
+              <Nav className="mr-auto" navbar>
+                <NavItem style={{marginRight: "5%"}}>
+                  <Link to="homeSection" className="nav-link" style={{fontSize: "18px", cursor: "pointer"}} spy={true} smooth={true} duration={1000}>HOME</Link>
+                </NavItem>
+                <NavItem style={{marginRight: "5%"}}>
+                  <Link to="aboutSection" className="nav-link" style={{fontSize: "18px", cursor: "pointer"}} offset={30} spy={true} smooth={true} duration={1000}>ABOUT</Link>
+                </NavItem>
+                <NavItem style={{marginRight: "5%"}}>
+                  <Link to="portfolioSection" className="nav-link" style={{fontSize: "18px", cursor: "pointer"}} offset={30} spy={true} smooth={true} duration={1000}>PORTFOLIO</Link>
+                </NavItem>
+                <NavItem style={{marginRight: "5%"}}>
+                  <Link to="blogSection" className="nav-link" style={{fontSize: "18px", cursor: "pointer"}} offset={-50} spy={true} smooth={true} duration={1000}>BLOG</Link>
+                </NavItem>
+                <NavItem style={{marginRight: "5%"}}>
+                  <Link to="footerSection" className="nav-link" style={{fontSize: "18px", cursor: "pointer"}} spy={true} smooth={true} duration={1000}>CONTACT</Link>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </div>
+        </Navbar>
+        <BackgroundImage fluid={headerImage.childImageSharp.fluid} style={{width: "98.9vw", height: "100vh"}}>
+        </BackgroundImage>
     </header>
   );
 }
